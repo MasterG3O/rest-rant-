17c0ea9d4aed2916a7da3cc1dc79601ac98d23c6
Geordanis Mondeja <gmondeja32@gmail.com>
2022-11-21 21:18:10 -0800

    day1

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
 
-// More code here in a moment
-router.get('/',(req,res)=>{
-    res.send ('GET/places')
+router.get('/', (req, res) => {
+    res.send('GET /places')
 })
 
 module.exports = router
