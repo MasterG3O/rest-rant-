2f57a317dec14dbb96c8098bde4d97a60c8859eb
Geordanis Mondeja <gmondeja32@gmail.com>
2022-12-02 21:06:14 -0800

    Show route connected

--- controllers/places_controllers.js
+++ controllers/places_controllers.js
@@ -6,6 +6,20 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show')
+  }
+})
+
+
 
 module.exports = router
 router.post('/', (req, res) => {
