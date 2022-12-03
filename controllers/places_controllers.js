e394f73c9fab24b05767de9ef2e96cebde4e86a0
Geordanis Mondeja <gmondeja32@gmail.com>
2022-12-02 21:47:47 -0800

    Added the edit form

--- controllers/places_controllers.js
+++ controllers/places_controllers.js
@@ -6,19 +6,19 @@ router.get('/', (req, res) => {
 })
 
 
-router.delete('/:id', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-    places.splice(id, 1)
-    res.redirect('/places')
+    res.render('places/edit', { place: places[id] })
   }
 })
+
 
 
 
