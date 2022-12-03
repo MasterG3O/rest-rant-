0213259ad33ad859da306796ebb9e572137f95c5
Geordanis Mondeja <gmondeja32@gmail.com>
2022-12-02 21:35:21 -0800

    Delete functionality implemented

--- controllers/places_controllers.js
+++ controllers/places_controllers.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 })
 
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
@@ -15,9 +15,11 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show')
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
+
 
 
 
--- views/default.jsx
+++ views/default.jsx
@@ -1,24 +1,35 @@
 const React = require('react')
 
-function Def (html) {
+function Def(html) {
     return (
         <html>
             <head>
                 <title>Home</title>
-              
-                <link rel="stylesheet" href="/css/style.css"/>
+
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+
+
                 <nav>
                     <ul>
                         <li>
-                            <a href ="/"> Home </a>
+                            <a href="/"> Home </a>
                         </li>
                         <li>
-                            <a href = "places">Places</a> 
+                            <a href="places">Places</a>
                         </li>
                         <li>
-                            <a href ="/places/new">Add Place</a>
+                            <a href="/places/new">Add Place</a>
                         </li>
                     </ul>
                 </nav>
