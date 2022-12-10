9d77186442e15dba6c0d1e5bf85586b7f63d2ed0
Geordanis Mondeja <gmondeja32@gmail.com>
2022-12-09 20:06:24 -0800

    Routes now working, new pages working

--- views/default.jsx
+++ views/default.jsx
@@ -1,42 +1,32 @@
 const React = require('react')
 
-function Def(html) {
+function Def (html) {
     return (
         <html>
             <head>
-                <title>Home</title>
-
-                <link rel="stylesheet" href="/css/style.css" />
+                <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
+            <nav>
+                <ul>
+                    <li>
+                    <a href="/">Home</a>
+                    </li>
+                    <li>
+                    <a href="/places">Places</a>
+                    </li>
+                    <li>
+                    <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+            </nav>
 
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
-
-
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="/"> Home </a>
-                        </li>
-                        <li>
-                            <a href="places">Places</a>
-                        </li>
-                        <li>
-                            <a href="/places/new">Add Place</a>
-                        </li>
-                    </ul>
-                </nav>
                 {html.children}
             </body>
         </html>
     )
-}
+  }
 
 module.exports = Def
--- views/home.jsx
+++ views/home.jsx
@@ -1,23 +1,24 @@
+
+
 const React = require('react')
 const Def = require('./default')
 
 function home () {
     return (
         <Def>
-          <main>
-    <h1>HOME</h1>
-    <div>
-      <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-      <div>
-        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-      </div>
-    </div>
-    <a href="/places">
-        <button className=" btn-primary">Places Page</button>
-    </a>
-// </main>
-
-//         </Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/salad-unsplash.jpg.jpg" alt="Chia Fruit Shake" />
+                  <div>
+                     Photo by <a href="https://unsplash.com/@pwign?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anh Nguyen</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                  </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
 }
 
