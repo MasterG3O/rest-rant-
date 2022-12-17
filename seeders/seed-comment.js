159b9bcc8d0e373161744f688fc8c5d398ce167a
Geordanis Mondeja <gmondeja32@gmail.com>
2022-12-16 21:10:39 -0800

    part 9 n 10

---
+++ seeders/seed-comment.js
@@ -0,0 +1,26 @@
+const db = require('../models')
+
+// To use await, we need an async function.
+async function seed() {
+    // Get the place, H-Thai-ML
+    let place = await db.Place.findOne({ name: 'H-Thai-ML' })
+
+   
+    let comment = await db.Comment.create({
+        author: 'Famished Fran',
+        rant: false,
+        stars: 5.0,
+        content: 'Wow, simply amazing! Highly recommended!'
+    })
+
+  
+    place.comments.push(comment.id)
+
+ 
+    await place.save()
+    
+    // Exit the program
+    process.exit()
+}
+
+seed()
